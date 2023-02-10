d86a8bac95df81570300df1da792a0c781032285
Ellie Richardson <egr27@byu.edu>
2023-02-10 10:51:36 -0700

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        // get info from context file to controller
+        private MovieFormContext _movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieFormContext X)
+        {
+            _logger = logger;
+            _movieContext = X;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts ()
+        {
+            return View("MyPodcasts");
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View("MovieForm");
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(FormResponse fr)
+        {
+            _movieContext.Add(fr);
+            _movieContext.SaveChanges();
+
+            return View("Confirmation", fr);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6/Models/MovieFormContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class  MovieFormContext : DbContext
+    {
+        // create constructor
+        public MovieFormContext (DbContextOptions<MovieFormContext> options) : base (options)
+        {
+
+        }
+        public DbSet<FormResponse> responses { get; set; }
+
+        // seed the database with 3 movies
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<FormResponse>().HasData(
+                new FormResponse
+                {
+                    FormId = 1,
+                    Category = "Comedy",
+                    Title = "The Proposal",
+                    Year = 2009,
+                    Director = "Anne Fletcher",
+                    Rating = "PG-13",
+                },
+
+                new FormResponse
+                {
+                    FormId = 2,
+                    Category = "Drama",
+                    Title = "27 Dresses",
+                    Year = 2008,
+                    Director = "Anne Fletcher",
+                    Rating = "PG-13",
+                },
+
+                new FormResponse
+                {
+                    FormId = 3,
+                    Category = "Comedy",
+                    Title = "The Other Woman",
+                    Year = 2014,
+                    Director = "Nick Cassavetes",
+                    Rating = "PG-13",
+                }
+                );
+        }
+    }
+}
